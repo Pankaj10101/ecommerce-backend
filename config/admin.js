f810da46367f70d2737ed665901fcdb77522b062
Pankaj10101 <rkay1048@gmail.com>
2023-06-10 10:46:33 +0530

    admin issue fix

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','vNLciRcAazxRsNX7nJ4uMg=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'yQZIKZbMyesjpam6oQ91/w=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT','px0zoMozlxTb7bTgTo8XCg=='),
     },
   },
 });
